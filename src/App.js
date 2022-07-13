6436d0d3c2af0e4a26e2c7712ae53229ca026c7a
fredriick <wayne.fred45@gmail.com>
2022-07-13 15:46:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
+import MatrixMultiplication from './components/MatrixMultiplication'
+const styles = (theme) => ({
+  message: {
+    margin: theme.spacing(2)
+  }
+})
+class App extends Component {
+  constructor(props) {
+    super(props)
+this.state = {
+      version: null,
+      pyodide: null
+    }
+  }
+componentDidMount() {
+    window
+      .loadPyodide({ indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.17.0/full/' })
+      .then((pyodide) => {
+        pyodide.runPythonAsync(`
+import sys
+sys.version
+`)
+          .then((version) => {
+            this.setState({ version, pyodide })
+          })
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+render() {
+    const { version, pyodide } = this.state
+    const { classes } = this.props
+return (
+      <div className={classes.message}>
+        {version == null
+          ? <Typography variant="h2">Loading Python</Typography>
+          : <MatrixMultiplication pyodide={pyodide} />
+        }
+      </div>
+    )
+  }
 }
-
-export default App;
+App.propTypes = {
+  classes: PropTypes.object
+}
+export default withStyles(styles)(App)
